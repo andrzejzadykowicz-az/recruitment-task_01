1ee99d4723e0d64fe50a24a439e3e33c645b6497
andrzejzadykowicz-az <andrzej.zadykowicz@gmail.com>
2026-06-08 06:39:47 +0000

    commit test

--- tests/pages/GooglePage.ts
+++ tests/pages/GooglePage.ts
@@ -2,6 +2,7 @@ import { Page, Locator } from '@playwright/test';
 
 export class GooglePage {
     readonly page: Page;
+    readonly page: Page2;
     readonly searchBox: Locator;
 
     constructor(page: Page) {
